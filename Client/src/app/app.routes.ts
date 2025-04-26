c504b59bfa3b07a0f4db86876ee823d40b286823
= <dinhhoang210903@gmail.com>
2025-04-26 21:31:43 +0700

    (feat): add NotFound and ServerError components

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { ProductDetailsComponent } from './features/products/product-details/product-details.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'account', loadChildren: () => import('./features/account/routes')
         .then(r => r.accountRoutes)},
-    {path: '**', redirectTo: '', pathMatch: 'full'},
+    { path: 'products/:slug', component: ProductDetailsComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: 'server-error', component: ServerErrorComponent },
+    {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ];
