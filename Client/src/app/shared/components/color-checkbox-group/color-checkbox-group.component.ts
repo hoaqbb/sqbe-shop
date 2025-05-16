5a9af9c203480e35cea44e1b44c96c878133ca88
= <dinhhoang210903@gmail.com>
2025-05-16 22:52:19 +0700

    (feat): add color and size checkbox group components with styling and functionality

---
+++ Client/src/app/shared/components/color-checkbox-group/color-checkbox-group.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Color } from '../../models/color';
+
+@Component({
+  selector: 'app-color-checkbox-group',
+  standalone: true,
+  imports: [CommonModule],
+  providers: [
+      {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => ColorCheckboxGroupComponent),
+        multi: true
+      }
+    ],
+  templateUrl: './color-checkbox-group.component.html',
+  styleUrl: './color-checkbox-group.component.css'
+})
+export class ColorCheckboxGroupComponent implements ControlValueAccessor{
+  @Input() colors: Color[] = [];
+
+  selectedIds: number[] = [];
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: number[]): void {
+    this.selectedIds = value || [];
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  toggleColor(id: number, checked: boolean): void {
+    const updated = [...this.selectedIds];
+    if (checked && !updated.includes(id)) {
+      updated.push(id);
+    } else if (!checked) {
+      const index = updated.indexOf(id);
+      if (index !== -1) updated.splice(index, 1);
+    }
+
+    this.selectedIds = updated;
+    this.onChange(this.selectedIds);
+  }
+}
---
+++ Client/src/app/shared/components/size-checkbox-group/size-checkbox-group.component.ts
@@ -0,0 +1,51 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Size } from '../../models/size';
+
+@Component({
+  selector: 'app-size-checkbox-group',
+  standalone: true,
+  imports: [],
+  providers: [
+        {
+          provide: NG_VALUE_ACCESSOR,
+          useExisting: forwardRef(() => SizeCheckboxGroupComponent),
+          multi: true
+        }
+      ],
+  templateUrl: './size-checkbox-group.component.html',
+  styleUrl: './size-checkbox-group.component.css'
+})
+export class SizeCheckboxGroupComponent implements ControlValueAccessor{
+  @Input() sizes: Size[] = [];
+
+  selectedIds: number[] = [];
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: number[]): void {
+    this.selectedIds = value || [];
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  toggleSize(id: number, checked: boolean): void {
+    const updated = [...this.selectedIds];
+    if (checked && !updated.includes(id)) {
+      updated.push(id);
+    } else if (!checked) {
+      const index = updated.indexOf(id);
+      if (index !== -1) updated.splice(index, 1);
+    }
+
+    this.selectedIds = updated;
+    this.onChange(this.selectedIds);
+  }
+}
