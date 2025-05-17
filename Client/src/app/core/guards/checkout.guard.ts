406f4aeb7b1fe756e120c06464e0ddebef643da0
= <dinhhoang210903@gmail.com>
2025-05-17 14:42:08 +0700

    (update): enhance checkout guard to load cart if empty

--- Client/src/app/core/guards/checkout.guard.ts
+++ Client/src/app/core/guards/checkout.guard.ts
@@ -2,16 +2,34 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { CartService } from '../services/cart.service';
 import { ToastrService } from 'ngx-toastr';
+import { firstValueFrom } from 'rxjs';
 
-export const checkoutGuard: CanActivateFn = (route, state) => {
+export const checkoutGuard: CanActivateFn = async (route, state) => {
   const cartService = inject(CartService);
   const router = inject(Router);
   const toastr = inject(ToastrService);
-  
-  if (!cartService.cart() || cartService.cart()?.cartItems.length === 0) {
-    toastr.error('Giỏ hàng bạn đang trống, vui lòng thêm sản phẩm vào giỏ hàng trước khi thanh toán.');
+
+  // Nếu chưa có cart hoặc cart rỗng => thử load giỏ hàng
+  if (
+    cartService.cart().id === '' ||
+    cartService.cart().cartItems.length === 0
+  ) {
+    try {
+      await firstValueFrom(cartService.getCart());
+    } catch (err) {
+      console.error('Failed to load cart:', err);
+      toastr.error('Không thể tải giỏ hàng. Vui lòng thử lại sau.');
+      router.navigateByUrl('/cart');
+      return false;
+    }
+  }
+  const cart = cartService.cart();
+  if (!cart || cart.cartItems.length === 0) {
+    toastr.error(
+      'Giỏ hàng bạn đang trống, vui lòng thêm sản phẩm vào giỏ hàng trước khi thanh toán!'
+    );
     router.navigateByUrl('/cart');
     return false;
-  } 
+  }
   return true;
 };
--- Client/src/app/features/admin/routes.ts
+++ Client/src/app/features/admin/routes.ts
@@ -1,10 +1,13 @@
 import { Route } from '@angular/router';
+import { ManageProductComponent } from './manage-product/manage-product.component';
 import { AdminComponent } from './admin.component';
 
 
 export const adminRoutes: Route[] = [
   {
     path: '',
-    component: AdminComponent
+    component: AdminComponent,
+    children: [
+      { path: 'product', component: ManageProductComponent },
   },
 ];
